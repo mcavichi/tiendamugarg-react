43c18cc0367d430baba065a4e0c031b25eb0011d
Marcos Cavichioli <mcavichi@gmail.com>
2022-11-21 18:12:31 -0300

    Aplicando JSX

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <h1 style={{color: 'red'}}>MyApp</h1>
+            <button className="boton">Boton 1</button>
+            <button className='boton'>Boton 2</button>
+            </nav>
+    )
+};
+
+export default Navbar;
